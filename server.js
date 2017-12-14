2baf7f849186db06d2040f21573e47c089e29f7c
John Cruickshank <johncruickshank87@gmail.com>
2017-12-14 13:52:19 +0000

    adds mongodb

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("client/build"));
 app.use(require(".controllers/index"));
 
+var MongoClient = require("mongodb").MongoClient;
+MongoClient.connect("mongodb://localhost:27017/gin_bars", function(err, client){
+  if(err){
+    return console.log(err);
+  }
+  db = client.db("gin_bars");
+  console.log("Connected to DB");
+
+  app.listen(3000, function () {
+    console.log("App running on port " + this.address().port);
+  });
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + "client/build/index.html");
