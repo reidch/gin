71dcc60adcc60bb6fdfaf40b496f5e179f04ec4e
Peter McCready <petermccready2@gmail.com>
2017-12-15 15:33:35 +0000

    adds addMarker function containing infoWindow and click event listener

--- client/src/models/mapWrapper.js
+++ client/src/models/mapWrapper.js
@@ -5,3 +5,34 @@ var MapWrapper = function(container, coords, zoom){
   });
   this.markers = [];
 }
+
+MapWrapper.prototype.addMarker = function(bar){
+  var marker = new google.maps.Marker({
+    position: bar.coords,
+    icon: "/icons/gnss.png",
+    infoWindowOpen: false,
+    map: this.googleMap
+  });
+
+  this.markers.push(marker);
+  var contentString = '<div id="content">'+
+  '<div id="bodyContent">'+
+  `<h3 id="bar-name">${bar.name}</h3>` +
+  `<h5>Location: ${bar.coords}</h5>`+
+  '</div>'+
+  '</div>';
+  marker.infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  marker.addListener('click', function(){
+    for (var mark of this.markers){
+      if (mark.infowindowOpen){
+        mark.infowindow.close();
+      }
+    }
+    marker.infowindow.open(this.googleMap, marker);
+    marker.infowindowOpen = true;
+  }.bind(this));
+
+}
