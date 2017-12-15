8a5a4aa89146be61df89c82401827de302ee6606
Peter McCready <petermccready2@gmail.com>
2017-12-15 15:46:57 +0000

    adds gin bars API request for all bar data

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,25 @@
+var makeRequest = function(url, callback){
+  var request = new XMLHttpRequest();
+  request.open("GET", url)
+  request.addEventListener('load', callback);
+  request.send();
+}
+
+var requestComplete = function(){
+  if (this.status !== 200) return console.log("Request failed");
+  console.log("Request successful")
+  var jsonString = this.responseText;
+  var apiData = JSON.parse(jsonString);
+  console.log(apiData);
+  // populateMap();
+}
+
+
+
 var app = function() {
   console.log("Running app");
+  var url = "/bars";
+  makeRequest(url, requestComplete);
 };
 
 window.addEventListener("load", app);
