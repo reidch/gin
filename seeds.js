da3298c65bc42e24638e330914466116f20af2ea
Caroline Reid <carolinehreid@gmail.com>
2017-12-15 11:11:18 +0000

    adds Gin71 Glasgow to seeds.js

--- seeds.js
+++ seeds.js
@@ -28,7 +28,7 @@ var bars = [
   		twitter_last_tweet: [],
   		social_media_links: {facebook: "https://www.facebook.com/gin71bar", instagram: "https://www.instagram.com/gin71bar/", twitter: "https://twitter.com/Gin71Bar"},
   		reviews: [],
-  		theme: "",
+  		theme: "Merchant City sophistication",
   		opening_times: {open: "17:00", closed: "00:00"},
   		phone_number: "0141 553 2326"
   	},
@@ -62,7 +62,36 @@ var bars = [
 	       opening_times: {open: "12:00", closed: "00:00"},
 	        phone_number: "0141 248 4165"
     },
-
+    {
+  		name: "Gin71 Glasgow",
+  		coords: { lat: 55.8632928, lng: -4.2574232 },
+  		address: "71 Renfield Street, Glasgow, G2 1LP",
+  		location: "Glasgow",
+  		description: "Glasgow's first dedicated gin bar. A beautiful space to enjoy carefully selected gins.",
+  		website: "https://www.gin71.com/aboutgla/",
+  		image: "http://1.bp.blogspot.com/-n1G_C4J7h6A/VkJR-0ab8kI/AAAAAAAAK3Y/qxKmnVFbfnk/s1600/11820661_1668436650104697_887626269_n.jpg",
+  		top3_gins: [{
+  			name: "Eden Mill Love",
+  			mixer: "Fever-Tree Tonic Water",
+  			price: 8.50
+  		},
+  		{
+  			name: "Lussa",
+  			mixer: "Gin71 Lemon & Rosemary Tonic",
+  			price: 9.50
+  		},
+  		{
+  			name: "Gin Mare",
+  			mixer: "Fever-Tree Mediterranean Tonic Water"
+  			price: 8.00
+  		}],
+  		twitter_last_tweet: [],
+  		social_media_links: {facebook: "https://www.facebook.com/gin71bar", instagram: "https://www.instagram.com/gin71bar/", twitter: "https://twitter.com/Gin71Bar"},
+  		reviews: [],
+  		theme: "",
+  		opening_times: {open: "17:00", closed: "00:00"},
+  		phone_number: "0141 353 2959"
+  	},
 ];
 
 db.bars.insertMany(bars);
