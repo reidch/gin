fefde1f0b61b8cd19944fb49adf5eee427236dae
Caroline Reid <carolinehreid@gmail.com>
2017-12-15 10:41:52 +0000

    adds Gin71 M City

--- seeds.js
+++ seeds.js
@@ -3,13 +3,35 @@ db.dropDatabase();
 
 var bars = [
   {
-    name: "Gin71",
-    location: { lat: 55.858711, lng: -4.250405 },
-    address: "No.4 Merchant City, Glasgow, Virginia Ct, Glasgow G1 1TN",
-    description: "Gin71 was Glasgow's first dedicated Gin Bar. The city's largest selection of Gins, home made tonics and cocktails.",
-    website: "https://www.gin71.com",
-    image: "https://images.itison.com/system/201611/78706/1480089437/original.jpg?w=572&h=378"
-  }
+  		name: "Gin71 Merchant City",
+  		coords: { lat: 55.8589196, lng: -4.2494644 },
+  		address: "40 Wilson Street, Glasgow, G1 1HD",
+  		location: "Glasgow",
+  		description: "",
+  		website: "https://www.gin71.com/aboutgmc/",
+  		image: "https://2.bp.blogspot.com/-SuvzXeyaQ4s/WNATuW_-vxI/AAAAAAAAKwk/3OYW2vOP1-w4SEPvBPdAtPQDeD4Fn1BbACEw/s1600/17409909_10158563424400294_2084261702_n.jpg",
+  		top3_gins: [{
+  			name: "Rock Rose",
+  			mixer: "Fever-Tree Tonic Water",
+  			price: "8.50"
+  		},
+  		{
+  			name: "Isle of Harris",
+  			mixer: "Gin71 Lemon & Rosemary Tonic",
+  			price: "9.50"
+  		},
+  		{
+  			name: "Arbikie AK’s Gin",
+  			mixer: "Gin71 Ginger Ale"
+  			price: "8.50"
+  		}],
+  		twitter_last_tweet: [],
+  		social_media_links: {facebook: "https://www.facebook.com/gin71bar", instagram: "https://www.instagram.com/gin71bar/", twitter: "https://twitter.com/Gin71Bar"},
+  		reviews: [],
+  		theme: "",
+  		opening_times: {open: "17:00", closed: "00:00"},
+  		phone_number: "0141 553 2326"
+  	},
 
 ];
 
