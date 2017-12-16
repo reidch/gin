2e855cf4776dd7ddb206b0bb2944eb88997c2dc0
Clare <chblackburne@gmail.com>
2017-12-16 16:43:19 +0000

    adds stars to info window

--- client/src/models/mapWrapper.js
+++ client/src/models/mapWrapper.js
@@ -16,10 +16,12 @@ MapWrapper.prototype.addMarker = function(bar){
   });
 
   this.markers.push(marker);
+  var star = '\u2605';
   var contentString = '<div id="content">'+
   '<div id="bodyContent">'+
   // `<h3 id="bar-name">${bar.rating}</h3>` +
   `<h3 class="bar-name">${bar.name}</h3>` +
+  `<h4 class="bar-rating">${star.repeat(3)}</h4>`+
   `<h5 class="open-time">Open: ${bar.opening_times.open}</h5>`+
   `<h5 class="closed-time">Closed: ${bar.opening_times.closed}</h5>`+
   '</div>'+
