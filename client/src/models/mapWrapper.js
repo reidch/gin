2e8d6f55ee89ec59c6463de649b14d41c1fce324
Clare <chblackburne@gmail.com>
2017-12-16 17:18:34 +0000

    adds bar-rating to multiply stars in map info window

--- client/src/models/mapWrapper.js
+++ client/src/models/mapWrapper.js
@@ -17,11 +17,14 @@ MapWrapper.prototype.addMarker = function(bar){
 
   this.markers.push(marker);
   var star = '\u2605';
+  var multiplier = bar.rating;
+  console.log(bar.rating);
+  console.log(bar.top3_gins[0].price)
   var contentString = '<div id="content">'+
   '<div id="bodyContent">'+
   // `<h3 id="bar-name">${bar.rating}</h3>` +
   `<h3 class="bar-name">${bar.name}</h3>` +
-  `<h4 class="bar-rating">${star.repeat(3)}</h4>`+
+  `<h4 class="bar-rating">${star.repeat(multiplier)}</h4>`+
   `<h5 class="open-time">Open: ${bar.opening_times.open}</h5>`+
   `<h5 class="closed-time">Closed: ${bar.opening_times.closed}</h5>`+
   '</div>'+
