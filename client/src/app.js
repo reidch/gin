ea57a9e7d457ba28ba75e0604c4afc40e468652e
John Cruickshank <johncruickshank87@gmail.com>
2017-12-16 15:31:00 +0000

    adds basic list functionality

--- client/src/app.js
+++ client/src/app.js
@@ -14,6 +14,7 @@ var requestComplete = function(){
   var apiData = JSON.parse(jsonString);
   console.log(apiData);
   populateMap();
+  populateList(apiData);
 }
 
 var populateMap = function(){
@@ -35,6 +36,49 @@ var populateMap = function(){
   mainMap.createSearchBox(input);
 
 }
+
+var populateList = function(data) {
+  var ul = document.getElementById("#list-header");
+  for (var bar of data) {
+    createBarData(bar);
+  }
+};
+// create the bar list data and append to the header
+var createBarData = function(bar) {
+  var barUl = document.getElementById("list-header")
+  var barLi = document.createElement("li-bar");
+  barLi.append(createBarName(bar.name));
+  barLi.append(createBarAddress(bar.address));
+  barLi.append(createBarDescription(bar.description));
+  barLi.append(createThumbnail(bar.image));
+  barUl.append(barLi);
+}
+
+var createBarName = function(name) {
+  var nameElement = document.createElement("bar-name");
+  nameElement.append(name);
+  return nameElement;
+};
+
+var createBarAddress = function(address) {
+  var addressElement = document.createElement("bar-address");
+  addressElement.append(address);
+  return addressElement;
+};
+
+var createBarDescription = function(description) {
+  var descriptionElement = document.createElement("bar-description");
+  descriptionElement.append(description);
+  return descriptionElement;
+};
+
+var createThumbnail = function(image) {
+  var thumbnailElement = document.createElement("bar-thumbnail");
+  var pic = document.createElement("img");
+  thumbnailElement.appendChild(pic);
+  pic.src = image;
+  return thumbnailElement;
+};
 
 var app = function() {
   console.log("Running app");
